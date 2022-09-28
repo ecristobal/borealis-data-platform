015079ee415c6274845581dec94533afb68e0994
Esteban Cristóbal <ecristobalr@gmail.com>
2022-09-28 21:54:44 +0200

    Fixed Snyk vulnerabilities

--- main.tf
+++ main.tf
@@ -22,17 +22,45 @@ provider "google" {
   zone    = "europe-southwest1-a"
 }
 
-resource "google_storage_bucket" "static" {
+resource "google_storage_bucket" "data-lake" {
   name                        = "borealis-data-lake"
   location                    = "EUROPE-SOUTHWEST1"
   storage_class               = "STANDARD"
   uniform_bucket_level_access = true
+  versioning {
+    enabled = true
+  }
+  logging {
+    log_bucket = "borealis-data-lake-logging"
+  }
+  encryption {
+    default_kms_key_name = google_kms_crypto_key.data-lake-sign-key.name
+  }
 }
 
-data "google_storage_bucket" "default" {
-  name = google_storage_bucket.static.id
+resource "google_kms_key_ring" "borealis-data-platform-keyring" {
+  name     = "borealis-data-platform"
+  location = "europe-southwest1"
+}
+
+resource "google_kms_crypto_key" "data-lake-sign-key" {
+  name     = "data-lake-sign"
+  key_ring = google_kms_key_ring.borealis-data-platform-keyring.id
+  purpose  = "ASYMMETRIC_SIGN"
+
+  version_template {
+    algorithm = "EC_SIGN_P384_SHA384"
+  }
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+data "google_storage_bucket" "data-lake" {
+  name = google_storage_bucket.data-lake.id
 }
 
 output "bucket_metadata" {
-  value = data.google_storage_bucket.default
+  value = data.google_storage_bucket.data-lake
 }
