e2346800fe7e949fb1f48c2fefb21fbc41b43402
Esteban Cristóbal <ecristobalr@gmail.com>
2022-09-29 21:11:17 +0200

    Reviewed formatting

--- main.tf
+++ main.tf
@@ -64,17 +64,13 @@ data "google_service_account" "terraform-account" {
 resource "google_kms_crypto_key_iam_binding" "bucket-binding" {
   crypto_key_id = google_kms_crypto_key.data-lake.id
   role          = "roles/cloudkms.cryptoKeyEncrypterDecrypter"
-  members       = [
-    "serviceAccount:${data.google_storage_project_service_account.data-lake-account.email_address}"
-  ]
+  members       = ["serviceAccount:${data.google_storage_project_service_account.data-lake-account.email_address}"]
 }
 
 resource "google_kms_crypto_key_ring_iam_binding" "terraform-binding" {
   key_ring_id = google_kms_key_ring.data-lake.id
   role        = "roles/cloudkms.admin"
-  members     = [
-    "serviceAccount:${data.google_service_account.terraform-account.email}"
-  ]
+  members     = ["serviceAccount:${data.google_service_account.terraform-account.email}"]
 }
 
 data "google_storage_bucket" "default" {
