5b12892c33a5f5d3cccb42b6653d4841c77bf77a
Esteban Cristóbal <ecristobalr@gmail.com>
2022-10-08 23:15:46 +0200

    Added Dataflow required storage

--- main.tf
+++ main.tf
@@ -24,7 +24,9 @@ provider "google" {
 
 data "google_project" "project" {}
 
-# Storage definition
+data "google_storage_project_service_account" "storage-account" {}
+
+# Data lake storage definition
 resource "google_storage_bucket" "data-lake" {
   name                        = "borealis-data-lake"
   location                    = "EUROPE-SOUTHWEST1"
@@ -40,26 +42,66 @@ resource "google_storage_bucket" "data-lake" {
   }
 
   encryption {
-    default_kms_key_name = google_kms_crypto_key.storage.id
+    default_kms_key_name = google_kms_crypto_key.data-lake.id
   }
 
-  depends_on = [google_kms_crypto_key_iam_binding.bucket-binding]
+  depends_on = [google_kms_crypto_key_iam_binding.data-lake-binding]
 
   labels = {
     "element" = "storage"
   }
 }
 
-data "google_storage_project_service_account" "data-lake-account" {}
-
-resource "google_kms_crypto_key_iam_binding" "bucket-binding" {
-  crypto_key_id = google_kms_crypto_key.storage.id
+resource "google_kms_crypto_key_iam_binding" "data-lake-binding" {
+  crypto_key_id = google_kms_crypto_key.data-lake.id
   role          = "roles/cloudkms.cryptoKeyEncrypterDecrypter"
-  members       = ["serviceAccount:${data.google_storage_project_service_account.data-lake-account.email_address}"]
+  members       = ["serviceAccount:${data.google_storage_project_service_account.storage-account.email_address}"]
 }
 
-resource "google_kms_crypto_key" "storage" {
-  name            = "storage"
+resource "google_kms_crypto_key" "data-lake" {
+  name            = "data-lake"
+  key_ring        = google_kms_key_ring.data-platform.id
+  rotation_period = "7776000s" # 90 days
+
+  labels = {
+    "element" = "storage"
+  }
+}
+
+# Dataflow storage definition
+resource "google_storage_bucket" "dataflow" {
+  name                        = "borealis-dataflow"
+  location                    = "EUROPE-SOUTHWEST1"
+  storage_class               = "STANDARD"
+  uniform_bucket_level_access = true
+
+  versioning {
+    enabled = true
+  }
+
+  logging {
+    log_bucket = "dataflow-logging"
+  }
+
+  encryption {
+    default_kms_key_name = google_kms_crypto_key.dataflow.id
+  }
+
+  depends_on = [google_kms_crypto_key_iam_binding.dataflow-binding]
+
+  labels = {
+    "element" = "storage"
+  }
+}
+
+resource "google_kms_crypto_key_iam_binding" "dataflow-binding" {
+  crypto_key_id = google_kms_crypto_key.dataflow.id
+  role          = "roles/cloudkms.cryptoKeyEncrypterDecrypter"
+  members       = ["serviceAccount:${data.google_storage_project_service_account.storage-account.email_address}"]
+}
+
+resource "google_kms_crypto_key" "dataflow" {
+  name            = "dataflow"
   key_ring        = google_kms_key_ring.data-platform.id
   rotation_period = "7776000s" # 90 days
 
