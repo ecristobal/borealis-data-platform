1d1d9c00f7069be0581962681f0e33bfae997a66
Esteban Cristóbal <ecristobalr@gmail.com>
2022-10-08 22:40:59 +0200

    Added new topic

--- main.tf
+++ main.tf
@@ -22,6 +22,7 @@ provider "google" {
   zone    = "europe-southwest1-a"
 }
 
+# Storage definition
 resource "google_storage_bucket" "data-lake" {
   name                        = "borealis-data-lake"
   location                    = "EUROPE-SOUTHWEST1"
@@ -41,17 +42,10 @@ resource "google_storage_bucket" "data-lake" {
   }
 
   depends_on = [google_kms_crypto_key_iam_binding.bucket-binding]
-}
 
-resource "google_kms_key_ring" "data-lake" {
-  name     = "data-lake"
-  location = "europe-southwest1"
-}
-
-resource "google_kms_crypto_key" "storage" {
-  name            = "storage"
-  key_ring        = google_kms_key_ring.data-lake.id
-  rotation_period = "7776000s" # 90 days
+  labels = {
+    "element" = "storage"
+  }
 }
 
 data "google_storage_project_service_account" "data-lake-account" {
@@ -61,4 +55,43 @@ resource "google_kms_crypto_key_iam_binding" "bucket-binding" {
   crypto_key_id = google_kms_crypto_key.storage.id
   role          = "roles/cloudkms.cryptoKeyEncrypterDecrypter"
   members       = ["serviceAccount:${data.google_storage_project_service_account.data-lake-account.email_address}"]
+  labels = {
+    "element" = "storage"
+  }
+}
+
+resource "google_kms_crypto_key" "storage" {
+  name            = "storage"
+  key_ring        = google_kms_key_ring.data-platform.id
+  rotation_period = "7776000s" # 90 days
+  labels = {
+    "element" = "storage"
+  }
+}
+
+# Topics definition
+resource "google_pubsub_topic" "exercises" {
+  name         = "borealis.data.input.exercises"
+  kms_key_name = google_kms_crypto_key.exercises.id
+  labels = {
+    "element" = "topic"
+  }
+}
+
+resource "google_kms_crypto_key" "exercises" {
+  name            = "exercises"
+  key_ring        = google_kms_key_ring.data-platform.id
+  rotation_period = "7776000s" # 90 days
+  labels = {
+    "element" = "topic"
+  }
+}
+
+# KMS definition
+resource "google_kms_key_ring" "data-platform" {
+  name     = "data-platform"
+  location = "europe-southwest1"
+  labels = {
+    "element" = "security"
+  }
 }
