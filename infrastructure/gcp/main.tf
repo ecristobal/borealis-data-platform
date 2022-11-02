5e281aa58cdca2925a7dae46b956efaa97f3e22c
Esteban Cristóbal <ecristobalr@gmail.com>
2022-11-02 21:40:06 +0100

    Updated Terraform scripts for GCP
    - Created unique module to set-up storage buckets
    - Updated main GCP script to use this module and create 4 buckets
    - Added object lifecycle to bucket settings

--- infrastructure/gcp/main.tf
+++ infrastructure/gcp/main.tf
@@ -18,8 +18,6 @@ provider "google" {
 
 data "google_project" "project" {}
 
-data "google_storage_project_service_account" "storage-account" {}
-
 # Settings to enable NewRelic monitoring
 resource "google_project_iam_member" "project" {
   project = data.google_project.project.id
@@ -36,50 +34,37 @@ resource "google_project_iam_binding" "project" {
   ]
 }
 
-# Data lake storage definition
-resource "google_storage_bucket" "data-lake" {
-  name                        = "borealis-data-lake"
-  location                    = "EUROPE-WEST3"
-  storage_class               = "STANDARD"
-  uniform_bucket_level_access = true
-
-  versioning {
-    enabled = true
-  }
-
-  logging {
-    log_bucket = "data-lake-logging"
-  }
-
-  encryption {
-    default_kms_key_name = google_kms_crypto_key.data-lake.id
-  }
-
-  depends_on = [google_kms_crypto_key_iam_binding.data-lake-binding]
-
-  labels = {
-    "element" = "storage"
-  }
-}
-
-resource "google_kms_crypto_key_iam_binding" "data-lake-binding" {
-  crypto_key_id = google_kms_crypto_key.data-lake.id
-  role          = "roles/cloudkms.cryptoKeyEncrypterDecrypter"
-  members       = ["serviceAccount:${data.google_storage_project_service_account.storage-account.email_address}"]
-}
-
-resource "google_kms_crypto_key" "data-lake" {
-  name            = "data-lake"
-  key_ring        = google_kms_key_ring.data-platform.id
-  rotation_period = "7776000s" # 90 days
-
-  labels = {
-    "element" = "storage"
-  }
-}
-
 # KMS definition
 resource "google_kms_key_ring" "data-platform" {
   name     = "data-platform"
   location = "europe-west3"
+}
+
+# Buckets definition
+module "bucket" {
+  source = "./storage"
+
+  storage-name    = "data-lake-landing"
+  kms-key-ring-id = google_kms_key_ring.data-platform.id
+}
+
+module "bucket" {
+  source = "./storage"
+
+  storage-name    = "landing-failed"
+  kms-key-ring-id = google_kms_key_ring.data-platform.id
+}
+
+module "bucket" {
+  source = "./storage"
+
+  storage-name    = "data-lake-staging"
+  kms-key-ring-id = google_kms_key_ring.data-platform.id
+}
+
+module "bucket" {
+  source = "./storage"
+
+  storage-name    = "staging-failed"
+  kms-key-ring-id = google_kms_key_ring.data-platform.id
 }
---
+++ infrastructure/gcp/storage/bucket.tf
@@ -0,0 +1,85 @@
+variable "storage-name" {
+  type        = string
+  description = "Storage name"
+}
+
+variable "kms-key-ring-id" {
+  type        = string
+  description = "Google Cloud KMS key ring ID"
+}
+
+data "google_storage_project_service_account" "storage-account" {}
+
+resource "google_storage_bucket" "data-lake" {
+  name                        = var.storage-name
+  location                    = "EUROPE-WEST3"
+  storage_class               = "STANDARD"
+  uniform_bucket_level_access = true
+
+  lifecycle_rule {
+    condition {
+      age                   = 30
+      matches_storage_class = "STANDARD"
+    }
+    action {
+      type          = "SetStorageClass"
+      storage_class = "NEARLINE"
+    }
+  }
+
+  lifecycle_rule {
+    condition {
+      age                   = 30
+      matches_storage_class = "NEARLINE"
+    }
+    action {
+      type          = "SetStorageClass"
+      storage_class = "COLDLINE"
+    }
+  }
+
+  lifecycle_rule {
+    condition {
+      age                   = 30
+      matches_storage_class = "COLDLINE"
+    }
+    action {
+      type          = "SetStorageClass"
+      storage_class = "ARCHIVE"
+    }
+  }
+
+  versioning {
+    enabled = false
+  }
+
+  logging {
+    log_bucket = "${var.storage-name}-logging"
+  }
+
+  encryption {
+    default_kms_key_name = google_kms_crypto_key.data-lake.id
+  }
+
+  depends_on = [google_kms_crypto_key_iam_binding.data-lake-binding]
+
+  labels = {
+    "element" = "storage"
+  }
+}
+
+resource "google_kms_crypto_key_iam_binding" "data-lake-binding" {
+  crypto_key_id = google_kms_crypto_key.data-lake.id
+  role          = "roles/cloudkms.cryptoKeyEncrypterDecrypter"
+  members       = ["serviceAccount:${data.google_storage_project_service_account.storage-account.email_address}"]
+}
+
+resource "google_kms_crypto_key" "data-lake" {
+  name            = "${var.storage-name}-key"
+  key_ring        = var.kms-key-ring-id
+  rotation_period = "7776000s" # 90 days
+
+  labels = {
+    "element" = "storage"
+  }
+}
